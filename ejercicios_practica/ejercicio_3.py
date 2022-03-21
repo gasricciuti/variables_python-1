da347eac76f85914cb255a908b025d9e6467ce04
gasricciuti <gas.ricciuti@gmail.com>
2022-03-21 20:54:17 -0300

    Realizado

--- ejercicios_practica/ejercicio_3.py
+++ ejercicios_practica/ejercicio_3.py
@@ -30,7 +30,9 @@ nombre_completo = nombre + ' ' + apellido
 # Imprimir la cantidad de letras que posee su nombre completo
 # cantidad_letras = len(....)
 
+nombre = nombre_completo.split(' ')
+cantidad_espacios = len(nombre) - 1
 cantidad_letras = len(nombre_completo)
 
-print('La cantidad de letras que posee su nombre completo es:', cantidad_letras - 1) # -1 por el espacio entre nombre y  apellido
+print('La cantidad de letras que posee su nombre completo es:', cantidad_letras - cantidad_espacios) # -1 por el espacio entre nombre y  apellido
 
